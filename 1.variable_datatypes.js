3a96b7ab40c651eaa44a9c7653b739d0ac081b95
alireza-hashemii <alirezahashemi.dev@gmail.com>
2025-07-18 09:42:51 +0330

    variables , datatypes , let and const usage

---
+++ 1.variable_datatypes.js
@@ -0,0 +1,19 @@
+// variables
+let FirstName = 'Alireza';
+let SecondName ='Hashemi';
+let BoolTesting = true;
+
+
+// reserved keywords are not allowed. Except prefixing it with $
+let $new = 'er';
+console.log($new);
+
+
+// in python we have None here instead undefined will be shown
+let v = console.log(FirstName);
+
+console.log(v);
+
+// using typeof will tell you the exact type of value stored in a variable
+console.log(typeof BoolTesting);
+
